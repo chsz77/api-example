8c597d8e784111eac5fd748c791c85d4c2b816d4
chsz77 <chsz77@gmail.com>
2018-06-01 14:45:02 -0400

    mongo

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://cahya:isra@ds235850.mlab.com:35850/webdev');
 
 mongoose.Promise = Promise;
 
